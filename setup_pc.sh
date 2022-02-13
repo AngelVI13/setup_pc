446b0d94f3938a1e5a11695c9eec525963262710
Angel ILIEV <a.v.iliev13@gmail.com>
2022-02-13 15:40:10 +0200

    Add initial files

---
+++ setup_pc.sh
@@ -0,0 +1,16 @@
+# Install vim, rg, fzf and other tools
+sudo dnf install -y vim-enhanced ripgrep fzf 
+
+# Install python tools globally
+sudo dnf install -y python3-black python3-flake8 python3-isort python3-mypy python3-ipython python3-ipdb
+
+# Add more tooling stuff here
+
+# Vim config setup
+cd ~
+# Remove any preexisting vimrc directory
+sudo rm -rf vimrc
+# Clone vim config repo
+git clone https://github.com/AngelVI13/vimrc.git 
+# Add global vimrc that links to the config from the git repo
+cp vimrc/.vimrc_top_level .vimrc
